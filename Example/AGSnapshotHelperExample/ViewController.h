8f46bdb93684cc17e0ff17326bb92646f66cb64c
ewa <ewa.bielska2@gmail.com>
2016-02-16 20:49:18 +0100

    Add exemplary project with tests

---
+++ Example/AGSnapshotHelperExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AGSnapshotHelperExample
+//
+//  Created by Ewa Bielska on 16/01/16.
+//  Copyright © 2016 Allegro Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Example/AGSnapshotHelperExample/ViewController.m
@@ -0,0 +1,13 @@
+//
+//  ViewController.m
+//  AGSnapshotHelperExample
+//
+//  Created by Ewa Bielska on 16/01/16.
+//  Copyright © 2016 Allegro Group. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@implementation ViewController
+
+@end
